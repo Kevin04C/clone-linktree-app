dfd6ce19e37781c49fde92ce959ae5a3da87d23a
Kevin04C <kevincespedes2004@gmail.com>
2023-03-17 22:46:23 -0500

    Add LinktreeRoutes

--- src/linktree/routes/LinktreeRoutes.tsx
+++ src/linktree/routes/LinktreeRoutes.tsx
@@ -1,5 +1,11 @@
 import type { FC } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { HomePage } from '../pages/HomePage'
 
 export const LinktreeRoutes: FC = () => {
-  return <div>LinktreeRoutes</div>
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
 }
