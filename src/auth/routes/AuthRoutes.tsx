2850bb241a4c25aaf21a7f9cca4a0471b7647804
Kevin04C <kevincespedes2004@gmail.com>
2023-03-17 22:46:46 -0500

    Add router register

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import type { FC } from 'react'
-import { LoginPage } from '../pages/LoginPage'
+import { LoginPage, RegisterPage } from '../pages/'
 
 export const AuthRoutes: FC = () => {
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
     </Routes>
   )
 }
