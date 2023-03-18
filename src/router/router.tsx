9494e8b6a303da8e4143446980d291cbb3058f0c
Kevin04C <kevincespedes2004@gmail.com>
2023-03-17 22:46:09 -0500

    Update router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,29 @@
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { LinktreeRoutes } from '../linktree/routes/LinktreeRoutes'
-import { Navigate, createBrowserRouter } from 'react-router-dom'
+import { Navigate, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { ProtectedRoutes } from './ProtectedRoutes'
+import { PublicRoutes } from './PublicRoutes'
 
-export const router = createBrowserRouter([
-  {
-    path: 'auth/*',
-    element: <AuthRoutes />
-  },
-  {
-    path: '/*',
-    element: <LinktreeRoutes />
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" />
-  }
-])
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route
+        path="auth/*"
+        element={
+          <PublicRoutes>
+            <AuthRoutes />
+          </PublicRoutes>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <ProtectedRoutes>
+            <LinktreeRoutes />
+          </ProtectedRoutes>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </>
+  )
+)
