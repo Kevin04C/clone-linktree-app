ab275c1083a3e059d578dd9b2f20ca666dd3e649
Kevin04C <kevincespedes2004@gmail.com>
2023-03-21 17:23:14 -0500

    Add route confirmation and redirect route

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import type { FC } from 'react'
-import { LoginPage, RegisterPage } from '../pages/'
+import { LoginPage, RegisterPage, SucessRegister } from '../pages/'
 
 export const AuthRoutes: FC = () => {
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
-      <Route path="register" element={<RegisterPage />} />
+      <Route path="register">
+        <Route index element={<RegisterPage />} />
+        <Route path="confirmation" element={<SucessRegister />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/auth/login" />} />
     </Routes>
   )
 }
