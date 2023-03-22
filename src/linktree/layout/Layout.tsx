4656b338de8d260bef3a832e8eb480c18857a824
Kevin04C <kevincespedes2004@gmail.com>
2023-03-22 15:47:39 -0500

    Add use Header

--- src/linktree/layout/Layout.tsx
+++ src/linktree/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import type { FC, ReactNode } from 'react'
+import { Header } from '../components/Header'
 
 interface Props {
   children: ReactNode
@@ -7,6 +8,7 @@ interface Props {
 export const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100">
+      <Header />
       <div className="md:p-2">{children}</div>
     </div>
   )
