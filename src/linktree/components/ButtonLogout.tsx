8cf89c61648abdc2a2116955223d6d7a989a8d93
Kevin04C <kevincespedes2004@gmail.com>
2023-03-22 15:40:01 -0500

    Add ButtonLogout

---
+++ src/linktree/components/ButtonLogout.tsx
@@ -0,0 +1,22 @@
+import type { FC, MouseEvent } from 'react'
+import { useStore } from '../../hooks/useStore'
+import { logout } from '../../store/auth/authSlice'
+import { CiLogout } from 'react-icons/ci'
+
+export const ButtonLogout: FC = () => {
+  const { dispatch } = useStore()
+  const handleLogout = (evt: MouseEvent<HTMLButtonElement>): void => {
+    dispatch(logout())
+    localStorage.removeItem('token')
+  }
+
+  return (
+    <button
+      className="w-full text-gray-700 flex gap-4 items-center hover:bg-stone-100 p-2 rounded-xl"
+      onClick={handleLogout}
+    >
+      <CiLogout />
+      Sign out
+    </button>
+  )
+}
