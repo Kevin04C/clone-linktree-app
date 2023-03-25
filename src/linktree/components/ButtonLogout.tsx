17ab239dbcc032e4a0c270f371a490b8cbef8356
Kevin04C <kevincespedes2004@gmail.com>
2023-03-25 00:46:04 -0500

    Update buttonLogo

--- src/linktree/components/ButtonLogout.tsx
+++ src/linktree/components/ButtonLogout.tsx
@@ -5,6 +5,7 @@ import { CiLogout } from 'react-icons/ci'
 
 export const ButtonLogout: FC = () => {
   const { dispatch } = useStore()
+
   const handleLogout = (evt: MouseEvent<HTMLButtonElement>): void => {
     dispatch(logout())
     localStorage.removeItem('token')
@@ -12,7 +13,7 @@ export const ButtonLogout: FC = () => {
 
   return (
     <button
-      className="w-full text-gray-700 flex gap-4 items-center hover:bg-stone-100 p-2 rounded-xl"
+      className="w-full text-gray-700 flex gap-4 items-center hover:bg-stone-100 px-2 py-4 rounded-xl"
       onClick={handleLogout}
     >
       <CiLogout />
