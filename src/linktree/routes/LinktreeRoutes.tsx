e801e3e42051e8c1dc654e2a3abecb849ce9a539
Kevin04C <kevincespedes2004@gmail.com>
2023-04-06 23:15:02 -0500

    Add more routes

--- src/linktree/routes/LinktreeRoutes.tsx
+++ src/linktree/routes/LinktreeRoutes.tsx
@@ -1,11 +1,29 @@
 import type { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { Layout } from '../layout/Layout'
 import { HomePage } from '../pages/HomePage'
+import { MorePage } from '../pages/MorePage'
+import { AppareancePage } from '../pages/AppareancePage'
+import { useStore } from '../../hooks/useStore'
+import { useEffect } from 'react'
+import { startGetLinks } from '../../store/clonetree/thunks'
 
 export const LinktreeRoutes: FC = () => {
+  const { dispatch } = useStore()
+
+  useEffect(() => {
+    dispatch(startGetLinks())
+  }, [])
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="more" element={<MorePage />} />
+        <Route path="appareance" element={<AppareancePage />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
