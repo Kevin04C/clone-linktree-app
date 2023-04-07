eed3675503f8be10a88d4d47af063073b9ecdaea
Kevin04C <kevincespedes2004@gmail.com>
2023-04-06 23:32:03 -0500

    Add using components

--- src/linktree/pages/HomePage.tsx
+++ src/linktree/pages/HomePage.tsx
@@ -1,11 +1,24 @@
 import type { FC } from 'react'
-import { Header } from '../components/Header'
-import { Layout } from '../layout/Layout'
+import { PhonePreview } from '../components/PhonePreview'
+import { AddNewLink } from '../components/AddNewLink'
+import { useStore } from '../../hooks/useStore'
+import { NewLinkForm } from '../views/NewLinkForm'
+import { LinksList } from '../components/LinksList'
 
 export const HomePage: FC = () => {
+  const { store } = useStore()
+  const { newLink } = store.cloneTree
   return (
-    <Layout>
-      <Header />
-    </Layout>
+    <section className="mt-2 md:mt-10 relative flex w-full md:gap-8 lg:gap-0">
+      <div className="w-full md:max-w-[640px] md:mx-auto">
+        <div className="px-4 md:px-0 mt-8">
+          {newLink ? <NewLinkForm /> : <AddNewLink />}
+          <LinksList />
+        </div>
+      </div>
+      <div className="hidden md:w-2/6 md:grid md:place-content-center">
+        <PhonePreview />
+      </div>
+    </section>
   )
 }
