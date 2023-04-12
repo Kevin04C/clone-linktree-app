2a0df0be8eed45ddb801a9ebcc58f6c0fe22ebeb
Kevin04C <kevincespedes2004@gmail.com>
2023-04-12 15:30:31 -0500

    Add Spinner when the header is loading

--- src/linktree/components/AddNewLink.tsx
+++ src/linktree/components/AddNewLink.tsx
@@ -3,9 +3,11 @@ import { Button } from '../../ui/Button'
 import { IoAdd } from 'react-icons/io5'
 import { useStore } from '../../hooks/useStore'
 import { newLink } from '../../store/clonetree/cloneTreeSlice'
+import { ClipLoader } from 'react-spinners'
 
 export const AddNewLink: FC = () => {
-  const { dispatch } = useStore()
+  const { dispatch, store } = useStore()
+  const { loading } = store.headers
 
   const handleNewLink = (): void => {
     dispatch(newLink())
@@ -16,8 +18,14 @@ export const AddNewLink: FC = () => {
       className="flex justify-center items-center gap-1 animate__animated animate__fadeInUp animate__faster"
       handleClick={handleNewLink}
     >
-      <IoAdd size={25} />
-      Add Link
+      {loading ? (
+        <ClipLoader size={25} color="#fff" />
+      ) : (
+        <>
+          <IoAdd size={25} />
+          Add Link
+        </>
+      )}
     </Button>
   )
 }
